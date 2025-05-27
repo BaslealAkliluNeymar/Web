e36175856898e71001fa68f55c5b86349f6cea3d
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2025-05-27 16:55:31 +0300

    message

--- src/features/Track/Selector.tsx
+++ src/features/Track/Selector.tsx
@@ -12,16 +12,16 @@ export default function SelectDemo() {
   return (
     <Select>
       <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
+        <SelectValue placeholder="Select Delivery Personnel" />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
           <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
+          <SelectItem value="Aynalem">Aynalem</SelectItem>
+          <SelectItem value="Mengistu">Mengistu</SelectItem>
+          <SelectItem value="Abebe">Abebe</SelectItem>
+          <SelectItem value="Almaz">Almaz</SelectItem>
+          <SelectItem value="Abel">Abel</SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
